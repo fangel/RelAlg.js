be33a8bf9c46b6f1166011aec28742eba9b15cd3
Morten Fangel <fangel@sevengoslings.net>
2013-09-05 16:31:53 +0200

    Added test-cases for the remaining of the Statement-cases. Criteria-cases still outstanding.

--- test/parser.js
+++ test/parser.js
@@ -17,39 +17,55 @@ function getStmt(expr) {
       assert.fail(expr.name, 'it', 'The expression is not on the form it := EXPR')
   else assert.fail(expr.constructor, 'Tree.Assignment', 'The expression is not on the form it := EXPR')
 }
-function binaryStmtMixin(terminal, ASTLeaf) {
-  it('Contains both the LHS and the RHS', function() {
+function binaryStmtMixin(terminal, ASTLeaf, glue) {
+  var msg = glue === undefined ? 'Contains both the LHS and the RHS' : 'Contains the LHS, the RHS and the operation meta-data'
+  it(msg, function() {
     var expr = Parser.parse("LHS " + terminal + " RHS")
       , stmt = getStmt(expr)
-      , expected = new ASTLeaf(new Tree.RelationReference('LHS'), new Tree.RelationReference('RHS'))
+    if (glue === undefined)
+      var expected = new ASTLeaf(new Tree.RelationReference('LHS'), new Tree.RelationReference('RHS'))
+    else  
+      var expected = new ASTLeaf(new Tree.RelationReference('LHS'), glue, new Tree.RelationReference('RHS'))
     assert.deepEqual(stmt, expected)
   })
 }
-function rightAssocMixin(terminal, ASTLeaf) {
+function rightAssocMixin(terminal, ASTLeaf, glue) {
   it('Should be right-associative', function() {
     var expr = Parser.parse("FST " + terminal + " SND " + terminal + " THD")
       , stmt = getStmt(expr)
-      , expected = new ASTLeaf(new Tree.RelationReference('FST'), new ASTLeaf(new Tree.RelationReference('SND'), new Tree.RelationReference('THD')))
+    if (glue === undefined)
+      var expected = new ASTLeaf(new Tree.RelationReference('FST'), new ASTLeaf(new Tree.RelationReference('SND'), new Tree.RelationReference('THD')))
+    else
+      var expected = new ASTLeaf(new Tree.RelationReference('FST'), glue, new ASTLeaf(new Tree.RelationReference('SND'), glue, new Tree.RelationReference('THD')))
     assert.deepEqual(stmt, expected)
   })
   it('.. and should respect parenthethis', function() {
     var expr = Parser.parse("(FST " + terminal + " SND) " + terminal + " THD")
       , stmt = getStmt(expr)
-      , expected = new ASTLeaf(new ASTLeaf(new Tree.RelationReference('FST'), new Tree.RelationReference('SND')), new Tree.RelationReference('THD'))
+    if (glue === undefined)
+      var expected = new ASTLeaf(new ASTLeaf(new Tree.RelationReference('FST'), new Tree.RelationReference('SND')), new Tree.RelationReference('THD'))
+    else
+      var expected = new ASTLeaf(new ASTLeaf(new Tree.RelationReference('FST'), glue, new Tree.RelationReference('SND')), glue, new Tree.RelationReference('THD'))
     assert.deepEqual(stmt, expected)        
   })
 }
-function leftAssocMixin(terminal, ASTLeaf) {
+function leftAssocMixin(terminal, ASTLeaf, glue) {
   it('Should be left-associative', function() {
     var expr = Parser.parse("FST " + terminal + " SND " + terminal + " THD")
       , stmt = getStmt(expr)
-      , expected = new ASTLeaf(new ASTLeaf(new Tree.RelationReference('FST'), new Tree.RelationReference('SND')), new Tree.RelationReference('THD'))
+    if (glue === undefined)
+      var expected = new ASTLeaf(new ASTLeaf(new Tree.RelationReference('FST'), new Tree.RelationReference('SND')), new Tree.RelationReference('THD'))
+    else
+      var expected = new ASTLeaf(new ASTLeaf(new Tree.RelationReference('FST'), glue, new Tree.RelationReference('SND')), glue, new Tree.RelationReference('THD'))
     assert.deepEqual(stmt, expected)
   })
   it('.. and should respect parenthethis', function() {
     var expr = Parser.parse("FST " + terminal + " (SND " + terminal + " THD)")
       , stmt = getStmt(expr)
-      , expected = new ASTLeaf(new Tree.RelationReference('FST'), new ASTLeaf(new Tree.RelationReference('SND'), new Tree.RelationReference('THD')))
+    if (glue === undefined)
+      var expected = new ASTLeaf(new Tree.RelationReference('FST'), new ASTLeaf(new Tree.RelationReference('SND'), new Tree.RelationReference('THD')))
+    else
+      var expected = new ASTLeaf(new Tree.RelationReference('FST'), glue, new ASTLeaf(new Tree.RelationReference('SND'), glue, new Tree.RelationReference('THD')))
     assert.deepEqual(stmt, expected)        
   })
 }
@@ -105,7 +121,9 @@ describe('Parsing', function(){
       leftAssocMixin.call(this, 'X', Tree.Cartesian)
     })
     describe('that is a join statement', function() {
-      it('Contains the LHS, RHS and join-criteria')
+      var criteria = new Tree.Criteria(new Tree.Value(1), '==', new Tree.Value(2))
+      binaryStmtMixin.call(this, 'Join[1==2]', Tree.Join, criteria)
+      rightAssocMixin.call(this, 'Join[1==2]', Tree.Join, criteria)
     })
     describe('that is a natural-join statement', function() {
       binaryStmtMixin.call(this, 'Join', Tree.NaturalJoin)
@@ -119,16 +137,39 @@ describe('Parsing', function(){
       //       right-assoc wins.
     })
     describe('that is contained in parenthethis', function() {
-      it('Should behave exactly as if the parenthethis weren\'t there')
+      it('Should behave exactly as if the parenthethis weren\'t there', function() {
+        var expr = Parser.parse('(REL)')
+          , stmt = getStmt(expr)
+          , expected = new Tree.RelationReference('REL')
+        assert.deepEqual(stmt, expected)
+      })
     })
     describe('that is a projection statement', function() {
-      it('Contains the projection list and the relation')
+      it('Contains the projection list and the relation', function() {
+        var expr = Parser.parse('Project[fst, snd](REL)')
+          , stmt = getStmt(expr)
+          , projList = new Tree.ProjectionList(['fst', 'snd'])
+          , expected = new Tree.Projection(projList, new Tree.RelationReference('REL'))
+        assert.deepEqual(stmt, expected)
+      })
     })
     describe('that is a rename statement', function() {
-      it('Contains the rename list and the relation')
+      it('Contains the rename list and the relation', function() {
+        var expr = Parser.parse('Rename[fst->FST, snd->SND](REL)')
+          , stmt = getStmt(expr)
+          , renameList = new Tree.RenameList([['fst', 'FST'], ['snd', 'SND']])
+          , expected = new Tree.Rename(renameList, new Tree.RelationReference('REL'))
+        assert.deepEqual(stmt, expected)
+      })
     })
     describe('that is a selection statement', function() {
-      it('Contains the selection criteria and the relation')
+      it('Contains the selection criteria and the relation', function() {
+        var expr = Parser.parse('Select[1==2](REL)')
+          , stmt = getStmt(expr)
+          , criteria = new Tree.Criteria(new Tree.Value(1), '==', new Tree.Value(2))
+          , expected = new Tree.Selection(criteria, new Tree.RelationReference('REL'))
+        assert.deepEqual(stmt, expected)
+      })
     })
   })
 })
