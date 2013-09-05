df9dfbfad9d8abcac2916dd021c5554ffec8b0a2
Morten Fangel <fangel@sevengoslings.net>
2013-09-05 17:35:20 +0200

    Added tests for criterias

--- test/parser.js
+++ test/parser.js
@@ -17,6 +17,13 @@ function getStmt(expr) {
       assert.fail(expr.name, 'it', 'The expression is not on the form it := EXPR')
   else assert.fail(expr.constructor, 'Tree.Assignment', 'The expression is not on the form it := EXPR')
 }
+function getCriteria(expr) {
+  var stmt = getStmt(expr)
+  if (stmt.criteria instanceof Tree.Criteria || stmt.criteria instanceof Tree.CriteriaComposition)
+    return stmt.criteria
+  else if (stmt.criteria !== undefined) assert.fail(stmt.criteria.constructor, 'Tree.Criteria(Composition)?', 'The criteria is not a real criteria')
+  else assert.fail(stmt.criteria, 'object', 'The expression doesn\'t have a criteria')
+}
 function binaryStmtMixin(terminal, ASTLeaf, glue) {
   var msg = glue === undefined ? 'Contains both the LHS and the RHS' : 'Contains the LHS, the RHS and the operation meta-data'
   it(msg, function() {
@@ -171,5 +178,121 @@ describe('Parsing', function(){
         assert.deepEqual(stmt, expected)
       })
     })
+  })
+  describe('Criterias', function() {
+    describe('that involves binary comparison', function() {
+      var ops = ['==', '!=', '<=', '>=', '<', '>']
+      for (op in ops) {
+        // Loop through all the different comparison operators we have
+        it('Works with ' + ops[op], function() {
+          var expected = new Tree.Criteria(new Tree.Value(1), ops[op], new Tree.Value(2))
+
+          // Check both places that could contain a criteria, in this case Select-operations
+          var expr = Parser.parse('Select[1 ' + ops[op] + ' 2](Foo)')
+            , criteria = getCriteria(expr)
+          assert.deepEqual(criteria, expected)
+
+          // And Join-operations
+          var expr = Parser.parse('LHS Join[1 ' + ops[op] + ' 2] RHS')
+            , criteria = getCriteria(expr)
+          assert.deepEqual(criteria, expected)
+        })
+      }
+    })
+    describe('supports types for the criteria fragments', function() {
+      var types = {
+        'integer': 1, // A integer, they are simple on the form [digits]
+        'string': '\'str\'', // Strings are '-escaped
+        'float': '1.0', // Floats are on the form [digits].[digits]
+        'attribute': 'attr' // A non-escaped string is used to refer to attributes on the relations
+      }
+      for (fst in types) {
+        for (snd in types) {
+          // And check that comparison between all 4 types work.
+          it('Has ' + fst + ' vs ' + snd + ' support', function() {
+            var lhs = fst == 'attribute' ? new Tree.Attribute(types[fst]) : new Tree.Value(types[fst])
+              , rhs = snd == 'attribute' ? new Tree.Attribute(types[snd]) : new Tree.Value(types[snd])
+              , expected = new Tree.Criteria(lhs, '==', rhs)
+
+            // Check both places that could contain a criteria, in this case Select-operations
+            var expr = Parser.parse('Select[' + types[fst] + ' == ' + types[snd] + '](Foo)')
+              , criteria = getCriteria(expr)
+            assert.deepEqual(criteria, expected)
+
+            // And Join-operations
+            var expr = Parser.parse('LHS Join[' + types[fst] + ' == ' + types[snd] + '] RHS')
+              , criteria = getCriteria(expr)
+            assert.deepEqual(criteria, expected)
+          })
+        }
+      }
+    })
+    describe('that is contained in parenthethis', function() {
+      it('Should behave as if there is no parenthethis', function() {
+        var expected = new Tree.Criteria(new Tree.Value(1), '==', new Tree.Value(2))
+        var expr = Parser.parse('Select[(1==2)](Foo)')
+          , criteria = getCriteria(expr)
+        assert.deepEqual(criteria, expected)
+
+        var expr = Parser.parse('LHS Join[(1==2)] RHS')
+          , criteria = getCriteria(expr)
+        assert.deepEqual(criteria, expected)
+      })
+    })
+    var comps = {
+      'conjunction': {terminal: '&&', op: 'AND'},
+      'disjunction': {terminal: '||', op: 'OR'}
+    }
+    for (comp in comps) {
+      describe('that is a ' + comp, function() {
+        it('Contains both the LHS and the RHS', function() {
+          var expected = new Tree.CriteriaComposition(
+            new Tree.Criteria(new Tree.Value(1), '==', new Tree.Value(2)),
+            comps[comp].op,
+            new Tree.Criteria(new Tree.Value(3), '==', new Tree.Value(4))
+          )
+          var expr = Parser.parse('Select[(1==2 ' + comps[comp].terminal + ' 3==4)](Foo)')
+            , criteria = getCriteria(expr)
+          assert.deepEqual(criteria, expected)
+          var expr = Parser.parse('LHS Join[(1==2 ' + comps[comp].terminal + ' 3==4)] RHS')
+            , criteria = getCriteria(expr)
+          assert.deepEqual(criteria, expected)
+        })
+        it('Should be left-associative', function() {
+          var expected = new Tree.CriteriaComposition(
+            new Tree.CriteriaComposition(
+              new Tree.Criteria(new Tree.Value(1), '==', new Tree.Value(2)),
+              comps[comp].op,
+              new Tree.Criteria(new Tree.Value(3), '==', new Tree.Value(4))
+            ),
+            comps[comp].op,
+            new Tree.Criteria(new Tree.Value(5), '==', new Tree.Value(6))
+          )
+          var expr = Parser.parse('Select[(1==2 ' + comps[comp].terminal + ' 3==4 ' + comps[comp].terminal + ' 5==6)](Foo)')
+            , criteria = getCriteria(expr)
+          assert.deepEqual(criteria, expected)
+          var expr = Parser.parse('LHS Join[(1==2 ' + comps[comp].terminal + ' 3==4 ' + comps[comp].terminal + ' 5==6)] RHS')
+            , criteria = getCriteria(expr)
+          assert.deepEqual(criteria, expected)
+        })
+        it('.. and should respect parenthethis', function() {
+          var expected = new Tree.CriteriaComposition(
+            new Tree.Criteria(new Tree.Value(1), '==', new Tree.Value(2)),
+            comps[comp].op,
+            new Tree.CriteriaComposition(
+              new Tree.Criteria(new Tree.Value(3), '==', new Tree.Value(4)),
+              comps[comp].op,
+              new Tree.Criteria(new Tree.Value(5), '==', new Tree.Value(6))
+            )
+          )
+          var expr = Parser.parse('Select[(1==2 ' + comps[comp].terminal + ' (3==4 ' + comps[comp].terminal + ' 5==6))](Foo)')
+            , criteria = getCriteria(expr)
+          assert.deepEqual(criteria, expected)
+          var expr = Parser.parse('LHS Join[(1==2 ' + comps[comp].terminal + ' (3==4 ' + comps[comp].terminal + ' 5==6))] RHS')
+            , criteria = getCriteria(expr)
+          assert.deepEqual(criteria, expected)
+        })
+      })
+    }
   })
 })
